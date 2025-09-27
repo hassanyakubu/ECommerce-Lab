9bdb063caf08bf1d526c88333c0a967def808f8d
Hassan Yakubu <yhassan677@gmail.com>
2025-09-27 17:37:11 +0000

    Category management with CRUD

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+require_once __DIR__ . '/settings/core.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,8 +20,15 @@
         <div class="container">
             <ul>
                 <li><a href="index.php">Home</a></li>
-                <li><a href="view/login.php" class="btn btn-secondary">Login</a></li>
-                <li><a href="view/register.php" class="btn btn-primary">Register</a></li>
+                <?php if (!isLoggedIn()): ?>
+                    <li><a href="view/login.php" class="btn btn-secondary">Login</a></li>
+                    <li><a href="view/register.php" class="btn btn-primary">Register</a></li>
+                <?php else: ?>
+                    <?php if (isAdmin()): ?>
+                        <li><a href="admin/category.php" class="btn btn-secondary">Category</a></li>
+                    <?php endif; ?>
+                    <li><a href="actions/logout_action.php" class="btn btn-danger">Logout</a></li>
+                <?php endif; ?>
             </ul>
         </div>
     </div>
@@ -29,13 +39,18 @@
             <p>This is a simple e-commerce platform. You can register for a new account or login if you already have one.</p>
             
             <div style="text-align: center; margin-top: 30px;">
-                <a href="view/register.php" class="btn btn-primary" style="margin-right: 10px;">Create Account</a>
-                <a href="view/login.php" class="btn btn-secondary">Sign In</a>
+                <?php if (!isLoggedIn()): ?>
+                    <a href="view/register.php" class="btn btn-primary" style="margin-right: 10px;">Create Account</a>
+                    <a href="view/login.php" class="btn btn-secondary">Sign In</a>
+                <?php else: ?>
+                    <?php if (isAdmin()): ?>
+                        <a href="admin/category.php" class="btn btn-secondary" style="margin-right: 10px;">Manage Categories</a>
+                    <?php endif; ?>
+                    <a href="actions/logout_action.php" class="btn btn-danger">Logout</a>
+                <?php endif; ?>
             </div>
         </div>
     </div>
 
 </body>
 </html>
-
-	
