7f4f4ee0d00da04fe009909b04944ad6b96f30fe
Hassan Yakubu <yhassan677@gmail.com>
2025-10-08 20:41:32 +0000

    db update

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,17 +1,8 @@
 <?php
-// Database credentials
-// Update these values according to your database configuration
+// Database credentials for LIVE SERVER
 
-// Database server hostname
 define('SERVER', 'localhost');
-
-// Database username
 define('USERNAME', 'hassan.yakubu');
-
-// Database password
-define('PASSWD', 'jonnytest');
-
-// Database name
-define('DATABASE', 'ecommerce_2025A_hassan_yakubu');
-
+define('PASSWD', 'jonnytes');
+define('DATABASE', 'ecommerce_2025A_hassan_yakubu'); 
 ?>
